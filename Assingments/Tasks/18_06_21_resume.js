d65355442c14e5a0bdb1ad388d515c9cbb47c878
Murali Akkina <muraliakkina@outlook.com>
2021-06-21 00:20:18 +0530

    Create 18_06_21_resume.js

---
+++ Assingments/Tasks/18_06_21_resume.js
@@ -0,0 +1,8 @@
+var resume = [
+    {"Information":[{'name':"Murali Sai Srinivas Akkina"},
+    {"address":[{'town':'Tanuku','state':'Andhra Pradesh','Country':'India','pincode':534216}]},{'emailid':'muraliakkina9@gmail.com'}]},
+    {'Education':[{'engineering':[{'degree':'B.E','stream':'Mechanical','cgpa':6.74,'passoutyear':2019}]},
+    {'highereducation':[{'stream':'MPC','%':93.2,'passoutyear':2015}]},{'ssc':[{'gpa':9.2,'passoutyear':2013}]}]},
+    {'hobbies':['tennis','cricket','news']},
+]
+console.log(resume)
