d20dc6fdf8e6a8312fd7aed890c6785a04bfb561
Murali Akkina <muraliakkina@outlook.com>
2021-06-22 09:49:08 +0530

    Update 21-06-21-task3.js

--- Assingments/Tasks/21-06-21-task3.js
+++ Assingments/Tasks/21-06-21-task3.js
@@ -1,7 +1,8 @@
+// Arrow Functions
 //Return all palindromes in an array
 var palinarray = ['anna','level','stats','oil','petrol',101,101101,144]
 var ne = [];
-var findPalin => (arg1){
+var findPalin = (arg1)=>{
     for(i of arg1){
         if(Number.isInteger(i)){
             i = i.toString()
@@ -26,3 +27,42 @@ var findPalin => (arg1){
     console.log(ne)    
 }
 findPalin(palinarray)
+
+
+//Return median of two sorted arrays of same size 
+var array1 = [1,10,11,12,5,6];
+var array2 = [2,7,3,8,4,9];
+var findmedian = (arg1,arg2)=>{
+    var newarray = [...arg1,...arg2];
+    var sortarray = newarray.sort((a,b)=>(a-b));
+    var middle = sortarray.length/2
+    return ((sortarray[middle])+(sortarray[middle-1]))/2;
+}
+console.log(findmedian(array1,array2))
+
+//Sum of all numbers in an Array
+var a = [1,2,3,4,5,6,7,8,9,10];
+var totalsum = 0;
+var sum = (xy)=>{
+    for(let i of a){
+        totalsum += i
+    }
+    console.log(totalsum)
+}
+sum(a)
+
+// To Convert all the strings to title caps in a string array
+var problem2 = 'i am using javascript'
+problem2 = problem2.split(' ')
+var bb = []
+var titleCase = (arg2)=>{
+    for(let i of arg2){
+        let j = i[0];
+        let k = j.toUpperCase();
+        i = i.replace(j,k)
+        bb.push(i)
+
+    }
+    console.log(bb)
+}
+titleCase(problem2)
